d95ec5a7b06c900a7aa10c4b546edb9efafd007f
nicoculebras <nicolas.robles@atos.net>
2020-06-18 23:08:17 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# saludo = "Hola "
+# nombre = input ("Ingrese su nombre: ")
+# print (saludo + nombre)
+
+num1 = int(input ("ingrese el primer numero: "))
+num2 = int(input ("ingrese el segundo numero: "))
+print( num1 + num2)
